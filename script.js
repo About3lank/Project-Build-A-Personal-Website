5c19222501b553d19e1229aa0dab0fa7c98a237b
Matthew Blank <matthew.3lank@gmail.com>
2021-08-19 19:14:57 -0400

    added Copy to clipboard buttons to table with JS functionality

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function changeText(id, newText) {
     var location = document.getElementById(id).querySelector('span.target');
     location.innerText = newText;
-}
+};
+
+
+function copyToClipboard(textToCopy) {
+    navigator.clipboard.writeText(textToCopy);
+    alert('Copied: "' + textToCopy + '"');
+};
